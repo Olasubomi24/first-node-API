e1c56e2663dbfd13b7db53548b9040f1ad931fd2
Olasubomi24 <profshubby@gmail.com>
2023-12-20 09:31:47 +0100

    pass jwrttoken

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -9,11 +9,12 @@ const {
 } = require("./user.controller");
 
 const router = require("express").Router();
+const {checkToken} = require("../../auth/token_validation")
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUsersByUserId);
-router.patch("/", updateUser);
-router.delete("/", deleteUser);
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id",checkToken, getUsersByUserId);
+router.patch("/",checkToken, updateUser);
+router.delete("/",checkToken, deleteUser);
 router.post("/login",login);
 module.exports = router;
